fcb463f87b6f1e19e2da58080985f2dc212f0358
rythgs <me@rythgs.co>
2020-02-21 20:19:51 +0900

    WIP: User API test

--- ormconfig.ts
+++ ormconfig.ts
@@ -12,7 +12,7 @@ const connectionOptions: ConnectionOptions = {
   database: config.db.database,
   synchronize: config.env === 'production' ? false : true,
   logging: false,
-  entities: ['src/models/**/*.ts'],
+  entities: ['src/models/index.ts'],
   migrations: ['src/migrations/**/*.ts'],
   subscribers: ['src/subscribers/**/*.ts'],
   cli: {
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import * as httpStatus from 'http-status'
 
-import { User } from '~/models/User'
+import { User } from '~/models'
 import { APIExceptionNotFound } from '~/utils/APIException'
 
 interface UserRequest extends Request {
