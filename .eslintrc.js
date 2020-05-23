cb8b32fb529582bf607f6eb29b3d6645c8136c9d
Ryota Higashi <me@higashiryota.com>
2020-05-23 21:15:43 +0900

    upgrade packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,51 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   globals: {
-    __DEV__: true
+    __DEV__: true,
   },
-  plugins: ["@typescript-eslint", "import", "simple-import-sort"],
+  plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: 'module',
   },
   rules: {
     // typescript
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/no-explicit-any": 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-explicit-any': 0,
     // prettier
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
-        trailingComma: "all",
-        semi: false
-      }
+        trailingComma: 'all',
+        semi: false,
+      },
     ],
     // common
-    "prefer-const": 2,
-    "no-unused-vars": 0,
-    "no-multiple-empty-lines": [2, { max: 1 }],
+    'prefer-const': 2,
+    'no-unused-vars': 0,
+    'no-multiple-empty-lines': [2, { max: 1 }],
     // import
-    "import/newline-after-import": 2,
-    "import/imports-first": 2,
-    "import/no-dynamic-require": 2,
-    "import/no-extraneous-dependencies": 2,
-    "import/no-mutable-exports": 2,
-    "import/no-commonjs": 2,
+    'import/newline-after-import': 2,
+    'import/imports-first': 2,
+    'import/no-dynamic-require': 2,
+    'import/no-extraneous-dependencies': 2,
+    'import/no-mutable-exports': 2,
+    'import/no-commonjs': 2,
     // import sort
-    "simple-import-sort/sort": 2
+    'simple-import-sort/sort': 2,
   },
   settings: {
     'import/resolver': {
       alias: {
-        map: [
-          ['~', './src'],
-        ],
+        map: [['~', './src']],
         extensions: ['.js', '.jsx', '.ts', '.tsx', 'json', '.d.ts'],
       },
     },
   },
-};
+}
